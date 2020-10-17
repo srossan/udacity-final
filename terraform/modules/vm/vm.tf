d9d0c2c99961e0c5085171d2b7ec8307911be146
Stephane Rossan <srossan@gmail.com>
2020-10-17 00:33:39 +0000

    adding details for mv

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "" {
+resource "azurerm_network_interface" "test" {
   name                = ""
   location            = var.location
   resource_group_name = var.resource_group
@@ -11,15 +11,15 @@ resource "azurerm_network_interface" "" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = "${var.application_type}-${var.resource_type}-sub"
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "${var.application_type}-${var.resource_type}-vm"
   location            = var.location
   resource_group_name = var.resource_group
   size                = "100"
   admin_username      = "admin"
   network_interface_ids = []
   admin_ssh_key {
-    username   = ""
+    username   = "admin"
     public_key = "file("~/.ssh/id_rsa.pub")"
   }
   os_disk {
