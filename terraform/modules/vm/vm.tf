d09a4156fb04d0fc030e5bf1d882fb3c65db3594
Stephane Rossan <srossan@gmail.com>
2020-10-17 00:55:23 +0000

    fixing ssh keys path

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -21,7 +21,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   ]
   admin_ssh_key {
     username   = "admin"
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
