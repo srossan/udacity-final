6a112c2cfc172db4cf7f9418d6f2f0b30f020bb2
Stephane Rossan <srossan@gmail.com>
2020-10-17 01:02:04 +0000

    fixing values

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -5,9 +5,9 @@ resource "azurerm_network_interface" "test" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = var.subnet_id_test
+    subnet_id                     = azurerm_subnet.test.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = var.public_ip_address_id
+    public_ip_address_id          = azurerm_public_ip.test.id
   }
 }
 
